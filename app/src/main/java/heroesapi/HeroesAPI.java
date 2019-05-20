d0221239c057ae8a93b669c67b216b52a8d442dd
Softwarica College Lab <softwarica@softwarica.com>
2019-05-20 11:40:03 +0545

    Add ImageResponse Class

--- app/src/main/java/heroesapi/HeroesAPI.java
+++ app/src/main/java/heroesapi/HeroesAPI.java
@@ -1,16 +1,29 @@
 package heroesapi;
 
+import java.util.List;
+
 import model.Heroes;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface HeroesAPI {
 
     @FormUrlEncoded
     @POST ("heroes")
     Call<Void> addHero (@Field("name") String name, @Field("desc") String desc );
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage (@Part MultipartBody.Part img);
+
+    @GET("heroes")
+    Call<List<Heroes>> getAllHeroes();
 
 }
