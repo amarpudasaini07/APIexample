41f1e8bb947303d9be95c77d1710b1f4ffa059fa
Softwarica College Lab <softwarica@softwarica.com>
2019-05-20 11:23:11 +0545

    Browse Image sucessfully

--- app/src/main/java/com/apiexample/MainActivity.java
+++ app/src/main/java/com/apiexample/MainActivity.java
@@ -1,12 +1,23 @@
 package com.apiexample;
 
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.net.URL;
 
 import heroesapi.HeroesAPI;
@@ -21,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     
     private EditText etName, etDesc;
     private Button btnSave;
+    private ImageView imgProfile;
+    String imagepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         etName=findViewById(R.id.etName);
         etDesc=findViewById(R.id.etDesc);
         btnSave=findViewById(R.id.btnSave);
+        imgProfile=findViewById(R.id.imgProfile);
         
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +51,54 @@ public class MainActivity extends AppCompatActivity {
                 Save();
             }
         });
+        
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+        });
+        
+    }
+
+    private void BrowseImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK){
+            if(data==null){
+                Toast.makeText(this, "PLease Select an Image", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        Uri uri=data.getData();
+        imagepath=getRealPathFromUri(uri);
+        previewImage(imagepath);
+    }
+
+    private void previewImage(String imagepath) {
+        File imgFile = new File(imagepath);
+        if (imgFile.exists()){
+            Bitmap mybitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            imgProfile.setImageBitmap(mybitmap);
+        }
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection ={MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, projection, null, null,null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result =cursor.getString(colIndex);
+        cursor.close();
+        return result;
     }
 
     private void Save() {
